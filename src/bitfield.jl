88d43fa6d321b81311c94458c68de9d7aae432f3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-04-01 19:29:42 -0400

    Update bitfield.jl

--- src/bitfield.jl
+++ src/bitfield.jl
@@ -8,7 +8,7 @@ BitField(::Type{T}, nbits::UInt16, offset::UInt16) where {T<:Base.BitUnsigned}
     if nbits == bitsof(T)
         BitField{T}(~zero(T), zero(UInt16), UInt16(bitsof(T))
     else
-        mask = nbitsone(T, n) << (bitsof(T) - offset)
+        mask = onebits(T, n) << (bitsof(T) - offset)
         BitField{T}(mask, offset, nbits)
     end
 end
