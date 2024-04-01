a9a1da11e2e24deed73574ebb3d1cf0b012d2c15
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-04-01 19:31:46 -0400

    Update bitops.jl

--- src/bitops.jl
+++ src/bitops.jl
@@ -1,7 +1,11 @@
 using Base: bitrotate, leading_zeros, trailing_zeros
 
+@inline bitsof(x) = sizeof(x) << 3
+
 @inline allzerobits(::Type{T}) where {T<:BitInteger} = zero(T)
 @inline allonebits(::Type{T}) where {T<:BitInteger} = ~zero(T)
+
+@inline onebits(::Type{T}, n) where {T<:BitInteger} = allonebits(T) >> max(0, (bitsof(T) - n))
 
 @inline masklsbs(n::T) where {T<:BitInteger} = (one(T) << n) - one(T)
 @inline masklsbs(::Type{T}, n) where {T<:BitInteger} = (one(T) << n) - one(T)
