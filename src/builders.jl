86302edb666810b7e2d71fd9f5dd12aa5c4084fe
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-04-02 17:52:57 -0400

    Update builders.jl

--- src/builders.jl
+++ src/builders.jl
@@ -1,4 +1,4 @@
-function bitfield(::Type{T}, spec::NamedTuple{(:name, :nbits, :offset), Tuple{Symbol, Int, Int}) where {T<:Base.BitUnsigned}
+function bitfield(::Type{T}, spec::NamedTuple{(:name, :nbits, :offset), Tuple{Symbol, Int, Int}}) where {T<:Base.BitUnsigned}
     fieldspec = BitFieldSpec(T; spec.nbits, spec.offset)
     NamedBitFieldSpec(spec.name, fieldspec)
 end
