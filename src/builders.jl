73e6980be9701bbf05ac33a95303346cde4d9870
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-04-02 14:17:25 -0400

    Update builders.jl

--- src/builders.jl
+++ src/builders.jl
@@ -2,3 +2,25 @@ function bitfield(::Type{T}, spec::NamedTuple{(:name, :nbits, :offset), Tuple{Sy
     fieldspec = BitFieldSpec(T; spec.nbits, spec.offset)
     NamedBitFieldSpec(spec.name, fieldspec)
 end
+
+function Base.sort(nt::NamedTuple)
+    syms = keys(nt)
+    vals = values(nt)
+    sperm = TT.sortperm(vals)
+    sortedsyms = Tuple([syms[i] for i in sperm])
+    sortedvals = [vals[i] for i in sperm]
+    NamedTuple{sortedsyms}(sortedvals)
+end
+
+# UNCHECKED PRECONDITION nt = sort(nt)
+function unsafe_overlap(nt::NamedTuple)
+    increments = first.(values(snt))[2:end] .- last.(values(snt))[1:end-1]
+    any(map(x->(x<1), increments))
+end
+
+function canonical(nt::NamedTuple)
+   snt = sort(nt)
+   unsafe_overlap(snt) && throw(ErrorException("bitfields must not overlap"))
+   snt
+end
+        
