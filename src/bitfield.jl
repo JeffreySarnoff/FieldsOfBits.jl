1e8a38d4444e5eb91c95052ce842cb0a2609375e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-04-01 22:42:15 -0400

    Update bitfield.jl

--- src/bitfield.jl
+++ src/bitfield.jl
@@ -1,30 +1,85 @@
-struct BitsField{T<:Base.BitUnsigned} <: Unsigned
+struct BitFieldSpec{T<:Base.BitUnsigned} <: Unsigned
     mask::T
     offset::UInt16
     nbits::UInt16
 end
 
-mutable struct BitField{{T<:Base.BitUnsigned} <: Unsigned
-    x::T
-    const field::BitsField
+mask(x::BitFieldSpec) = x.mask
+offset(x::BitFieldSpec) = x.offset
+nbits(x::BitFieldSpec) = x.nbits
+
+Base.leading_zeros(x::BitFieldSpec) = leading_zeros(mask(x))
+Base.trailing_zeros(x::BitFieldSpec) = offset(x)
+
+function BitFieldSpec(mask::T) where {T<:Base.BitUnsigned}
+     offset = UInt16(trailing_zeros(mask))
+     nbits  = UInt16(trailing_ones(mask >> offset))
+     BitFieldSpec(mask, offset, nbits)
 end
 
-BitsField(::Type{T}, nbits::UInt16, offset::UInt16) where {T<:Base.BitUnsigned}
-    if nbits == bitsof(T)
-        BitField{T}(~zero(T), zero(UInt16), UInt16(bitsof(T))
-    else
-        mask = onebits(T, n) << (bitsof(T) - offset)
-        BitField{T}(mask, offset, nbits)
-    end
+function BitFieldSpec(::Type{T}; offset, nbits) where {T<:Base.BitUnsigned}
+    (bitsof(T) < nbits + offset) && throw(DomainError("$(bitsof(T)) < offset + nbits ($(offset) + $(nbits))"))
+    nbits == 0 && return throw(DomainError("nbits must be > 0"))
+    nbits == bitsof(T) && return ones(T)
+    mask = (one(T) << nbits - one(T)) << offset
+    BitFieldSpec(mask, offset, nbits)
+end
+
+function Base.show(io::IO, x::BitFieldSpec)
+    mask = x.mask
+    offset = x.offset
+    nbits = x.nbits
+    nt = (;mask, offset, nbits)
+    show(io, nt)
+end
+
+@inline function validate(bfs::BitFieldSpec{T}, x::T) where {T}
+    leading_zeros(x) >= bitsof(T) - bfs.nbits
+end
+
+mutable struct BitField{T<:Base.BitUnsigned} <: Unsigned
+    content::T
+    const spec::BitFieldSpec
+end
+
+content(x::BitField{T}) where {T} = x.content
+spec(x::BitField{T}) where {T} = x.spec
+
+Base.leading_zeros(x::BitField) = leading_zeros(spec(x))
+Base.trailing_zeros(x::BitField) = trailing_zeros(spec(x))
+
+function BitField(spec::BitFieldSpec{T}) where {T}
+    BitField(zero(T), spec)
+end
+
+@inline function Base.get(x::BitField{T}) where {T}
+    x.content
+end
+
+@inline function unsafe_set!(x::BitField(T), content::T) where {T}
+    x.content = content
+end
+
+@inline function Base.set!(x::BitField(T), content::T) where {T}
+    !validate(x.spec, content) && throw(DomainError("cannot set $(x) to $(content)"))
+    x.content = content
+end
+
+function Base.show(io::IO, x::BitField)
+    content = x.val
+    quantity = content << x.spec.offset
+    mask = x.spec.mask
+    nt = (;quantity, content, mask)
+    show(io, nt)
 end
 
 @inline isolate(x::BitField{T}) where {T<:Base.BitUnsigned} =
-   (x.x & x.field.mask)
+   (x.x & x.spec.mask)
 
 @inline intolsbs(x::BitField{T}) where {T<:Base.BitUnsigned} =
-    isolate(x) >> x.field.offset
+    isolate(x) >> x.spec.offset
 
 @inline fromlsbs(x::T, b::BitField{T}) where {T<:Base.BitUnsigned} =
-  b.x = (x << b.field.offset) & b.field.mask
+  b.x = (x << b.spec.offset) & b.spec.mask
 
 
