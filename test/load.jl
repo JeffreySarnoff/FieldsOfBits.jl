6f63ec620108d55656b8d89fc30097a04775ce41
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-04-26 13:09:57 -0400

    cleanup

--- test/load.jl
+++ test/load.jl
@@ -176,7 +176,6 @@ shift the newvalue into position, replace value(x)
 end
 
 # ======================================
-
 """
     struct NamedBitFields
 
@@ -190,12 +189,18 @@ struct NamedBitFields{N,T<:BitInteger}
     ids::NTuple{N,Symbol}
 end
 
+function NamedBitFields(basic::BasicBitFields{N,T}, symbols::Tuple{Vararg{Symbol,N}}) where {N,T}
+    themasks = masks(basic)
+    theshifts = shifts(basic)
+    NamedBitFields{N,T}(themasks, theshifts, symbols)
+end
+
 function NamedBitFields(::Type{T}, ids::NTuple{N,Symbol}, bitmasks::NTuple{N,<:Unsigned}) where {N,T<:BitInteger}
     bitshifts = map(a -> Int8(trailing_zeros(a)), bitmasks)
     NamedBitFields{N,T}(bitmasks, bitshifts, ids)
 end
 
-function NamedBitFields(::Type{T}, ids::NTuple{N,Symbol}, bitspans::NTuple{N,<:Signed}) where {N,T}
+function NamedBitFields(::Type{T}, ids::NTuple{N,Symbol}, bitspans::NTuple{N,S}) where {N,T,S}
     NamedBitFields(T, masks_from_spans(T, bitspans), ids)
 end
 
@@ -203,7 +208,7 @@ Base.fieldcount(x::NamedBitFields{N,T}) where {N,T} = N
 
 masks(x::NamedBitFields) = getfield(x, :masks)
 shifts(x::NamedBitFields) = getfield(x, :shifts)
-ids(x::NamedBitFields) = getfield(x.:ids)
+ids(x::NamedBitFields) = getfield(x, :ids)
 
 mask(x::NamedBitFields, i) = @inbounds masks(x)[i]
 shift(x::NamedBitFields, i) = @inbounds shifts(x)[i]
@@ -215,7 +220,8 @@ function Base.NamedTuple(x::NamedBitFields{N,T}) where {N,T}
 end
 
 function Base.show(io::IO, x::NamedBitFields{N,T}) where {N,T}
-    str = string(NamedTuple(x))
+    nt = NamedTuple{ids(nbf)}(zip(masks(nbf), shifts(nbf)))
+    str = string("NamedBitFields{", N, ",", T, "}", nt)
     print(io, str)
 end
 
