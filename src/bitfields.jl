125296a3f673a8ca8245ddd2a00aa1c257dfde25
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-04-26 05:47:29 -0400

    improve

--- src/bitfields.jl
+++ src/bitfields.jl
@@ -1,13 +1,13 @@
 struct BitFields{N,T<:BitInteger} <: Integer
     value::T
-    fields::NamedBitFields{N,T}
+    bitfields::NamedBitFields{N,T}
 end
 
 value(x::BitFields) = x.value
-fields(x::BitFields) = x.fields
-masks(x::BitFields) = x.fields.masks
-shifts(x::BitFieldSpec) = x.fields.shifts
-names(x::BitFieldSpec) = x.fields.names
+bitfields(x::BitFields) = x.bitfields
+masks(x::BitFields) = x.bitfields.masks
+shifts(x::BitFields) = x.bitfields.shifts
+syms(x::BitFields) = x.bitfields.syms
 
 mask(x::BitFields, i) = @inbounds masks(x)[i]
 shift(x::BitFields, i) = @inbounds shift(x)[i]
@@ -18,38 +18,32 @@ bitwidth(x::BitFields{N,T}, i) where {N,T} = bitsof(T) - leading_zeros(masklsbs(
 
 Base.eltype(x::BitFields{N,T}) where {N,T} = T
 
-
-
-
-function BitFieldSpec(name::Symbol, mask::T) where {T<:Base.BitUnsigned}
-    shift = UInt16(trailing_zeros(mask))
-    width = UInt16(trailing_ones(mask >> shift))
-    BitFieldSpec(mask, shift % UInt16, width % UInt16, name)
+function Base.getproperty(bfs::BitFields{N,T}, name::Symbol) where {N,T}
+    idx = findfirst(===(name), names(bfs))
+    (mask(bfs, idx), shift(bfs, idx))
 end
 
-function BitFieldSpec(::Type{T}; name::Symbol, width, shift) where {T<:Base.BitUnsigned}
-    (bitsof(T) < width + shift) && throw(DomainError("$(bitsof(T)) < shift + width ($(shift) + $(width))"))
-    width == 0 && return throw(DomainError("width must be > 0"))
-    width == bitsof(T) && return ones(T)
-    mask = (one(T) << width - one(T)) << shift
-    BitFieldSpec(mask, shift % UInt16, width % UInt16, name)
+
+BitFields(nbf::NamedBitFields{N,T}) where {N,T} =
+    BitFields{N,T}(zero(T), nbf)
+
+function BitFields(::Type{T}, nbf::NamedBitFields{N,T}) where {N,T}
+    BitFields{N,T}(zero(T), nbf)
 end
 
-function Base.show(io::IO, x::BitFieldSpec)
-    str = string(x)
+function BitFields(::Type{T}, names::NTuple{N,Symbol}, bitmasks::NTuple{N,<:Unsigned}) where {N,T<:BitInteger}
+    namedbitfields = NamedBitFields(T, names, bitmasks)
+    BitFields{N,T}(zero(T), namedbitfields)
+end
+
+function BitFields(::Type{T}, names::NTuple{N,Symbol}, bitspans::NTuple{N,<:Signed}) where {N,T}
+    namedbitfields = NamedBitFields(T, names, bitspans)
+    BitFields{N,T}(zero(T), namedbitfields)
+end
+
+function Base.show(io::IO, x::BitFields)
+    valstr = string(x.value)
+    str = valstr * ": " * string(NamedTuple(x.bitfields))
     print(io, str)
-end
-
-function Base.string(x::BitFieldSpec)
-    mask = x.mask
-    shift = Int16(x.shift)
-    width = Int16(x.width)
-    name = x.name
-    spec = (; name, mask, shift, width)
-    string(spec)
-end
-
-@inline function validate(bfs::BitFieldSpec{T}, x::T) where {T}
-    leading_zeros(x) >= bitsof(T) - bfs.width
 end
 
--- src/named_bitfields.jl
+++ src/named_bitfields.jl
@@ -3,35 +3,39 @@
 
 - const field `masks`
 - const field `shifts`
-- const field `names`
+- const field `syms`
 """
 struct NamedBitFields{N, T<:BitInteger}
     masks::NTuple{N,T}
     shifts::NTuple{N,Int8}
-    names::NTuple{N,Symbol}
+    syms::NTuple{N,Symbol}
 end
 
-function NamedBitFields(::Type{T}, names::NTuple{N,Symbol}, bitmasks::NTuple{N,T}) where {N,T<:BitInteger}
+function NamedBitFields(::Type{T}, syms::NTuple{N,Symbol}, bitmasks::NTuple{N,<:Unsigned}) where {N,T<:BitInteger}
     bitshifts = map(a -> Int8(trailing_zeros(a)), bitmasks)
-    NamedBitFields{N,T}(bitmasks, bitshifts, names)
+    NamedBitFields{N,T}(bitmasks, bitshifts, syms)
 end
 
-function NamedBitFields(::Type{T}, names::NTuple{N,Symbol}, bitspans::NTuple{N,<:Signed}) where {N,T}
-    NamedBitFields(T, masks_from_spans(T, bitspans), names)
+function NamedBitFields(::Type{T}, syms::NTuple{N,Symbol}, bitspans::NTuple{N,<:Signed}) where {N,T}
+    NamedBitFields(T, masks_from_spans(T, bitspans), syms)
 end
 
 Base.fieldcount(x::NamedBitFields{N,T}) where {N,T} = N
 
 masks(x::NamedBitFields) = x.masks
 shifts(x::NamedBitFields) = x.shifts
-names(x::NamedBitFields) = x.names
+syms(x::NamedBitFields) = x.syms
 
 mask(x::NamedBitFields, i) = @inbounds x.masks[i]
 offset(x::NamedBitFields, i) = @inbounds x.shifts[i]
-name(x::NamedBitFields, i) = @inbounds x.names[i]
+sym(x::NamedBitFields, i) = @inbounds x.syms[i]
 masklsbs(x::BasicBitFields, i) = @inbounds x.masks[i] >> x.shifts[i]
 
 function Base.NamedTuple(x::NamedBitFields{N,T}) where {N,T}
     NamedTuple{names(x)}( collect(zip(masks(x), shifts(x))))
 end
 
+function Base.show(io:IO, x::NamedBitFields{N,T}) where {N,T}
+    str = string(NamedTuple(x))
+    print(io, str)
+end
